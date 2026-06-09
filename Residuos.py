c26575ba258526c79b9c8b969c3f832c06ec19d6
jblanquilloyucra-hue <jblanquilloyucra@gmail.com>
2026-06-09 00:24:32 -0500

    "Analisis Exploratorio"

--- Residuos.py
+++ Residuos.py
@@ -9,3 +9,28 @@ df = pd.read_csv("1. DataSet Generación Anual de residuos sólidos domiciliario
 print(df.head())
 print(df.info())
 print(df.isnull().sum())
+# ANALISIS EXPLORATORIO
+
+
+# Estadísticas descriptivas
+print(df.describe())
+
+# Revisar años disponibles
+print(df["periodo"].unique())
+
+# Generación total por año
+generacion_anual = df.groupby("periodo")["qresiduos_dom"].sum()
+print(generacion_anual)
+
+# Generación total por departamento
+generacion_departamento = df.groupby("departamento")["qresiduos_dom"].sum().sort_values(ascending=False)
+print(generacion_departamento)
+
+# Generación promedio por región natural
+residuos_por_region = (
+    df.groupby("reg_nat")["qresiduos_dom"]
+    .mean()
+    .sort_values(ascending=False)
+)
+
+print(residuos_por_region)
